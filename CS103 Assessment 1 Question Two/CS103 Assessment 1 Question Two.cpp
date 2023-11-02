2d019b34faca774cbb1a511a824930f10993ba68
Matt T <mrsymtec@gmail.com>
2023-11-02 15:09:16 +1300

    Add project files.

---
+++ CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
@@ -0,0 +1,14 @@
+//CS103 Assessment One Question Two
+//Student Name: Matt Taylor
+// This program sums the non-negative values in a list as per the brief 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+
+	return 0;
+}
+
