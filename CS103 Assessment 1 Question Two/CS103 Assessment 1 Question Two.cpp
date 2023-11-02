1c026877ba82719699b7410cb451ec9de514dd9f
Matt T <mrsymtec@gmail.com>
2023-11-02 18:19:21 +1300

    Trying to correct an error in the code.

--- CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
+++ CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
@@ -1,25 +1,43 @@
 //CS103 Assessment One Question Two
 //Student Name: Matt Taylor
-// This program sums the non-negative values in a list as per the brief 
+//This program sums the non-negative values in a list as per the brief 
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-//Creation of array and size function that takes arry and size as parameters
-//The calculation of the non negatives is here as well. 
+// Function to calculate the sum of non-negative values in an array 
 int sunNonNegative(int* arr, int size) {
-	int sum = 0;
+	int sum = 0; // Initialize a variable to store the sum
+// Iterate through the elements of the array
 	for (int i = 0; i < size; i++) {
-		if (arr[i] > = 0) {
+// Check if the current element is non-negative
+		if (arr[i] >= 0) {
+// If the element is non-negative, add it to the sum
 			sum += arr[i];
 		}
 	}
-	return sum;
+	return sum; // Return the sum of non-negative values
 }
 
 int main()
 {
-   
+	int size;
+//Asks the user to enter the size of the list
+	cout << "Enter the size of the List: ";
+	cin >> size;
+
+	vector <int> list(size); //Declares the size of the arry at a  specified size
+
+//Prompts the user to enter the elements of the array
+	cout << "Enter" << size << " elements:" << std::endl;
+	for (int i = 0; i < size; i++) {
+		cin >> list[i]; //Reads the elements from the user
+	}
+
+//This calls the sumNonNegative function to calculate the sum of non-negative values
+	int result = sunNonNegative(list, size);
+	cout << "Sum of non-negative-values: " << result << std::endl;
 
 
 	return 0;
