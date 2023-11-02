76f2023bfcc1402dcece97a0f2fc0d008fa96919
Matt T <mrsymtec@gmail.com>
2023-11-02 15:19:38 +1300

    Creation of the function completed.

--- CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
+++ CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
@@ -5,6 +5,18 @@
 #include <iostream>
 using namespace std;
 
+//Creation of array and size function that takes arry and size as parameters
+//The calculation of the non negatives is here as well. 
+int sunNonNegative(int* arr, int size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > = 0) {
+			sum += arr[i];
+		}
+	}
+	return sum;
+}
+
 int main()
 {
    
