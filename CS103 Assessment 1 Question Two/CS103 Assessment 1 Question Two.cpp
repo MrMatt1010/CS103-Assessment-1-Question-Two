59fbb41b2235844563f9e26dc6b6afb0216f594a
Matt T <mrsymtec@gmail.com>
2023-11-06 09:19:49 +1300

    corrected function name from "sunNonNegative" to "sumNonNegative" and changed the function's parameter to accept a vector<int>& instead of an array pointer. added a missing space in the "Enter" line to display the correct prompt for entering elements.

--- CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
+++ CS103 Assessment 1 Question Two/CS103 Assessment 1 Question Two.cpp
@@ -7,39 +7,37 @@
 using namespace std;
 
 // Function to calculate the sum of non-negative values in an array 
-int sunNonNegative(int* arr, int size) {
-	int sum = 0; // Initialize a variable to store the sum
-// Iterate through the elements of the array
-	for (int i = 0; i < size; i++) {
-// Check if the current element is non-negative
-		if (arr[i] >= 0) {
-// If the element is non-negative, add it to the sum
-			sum += arr[i];
-		}
-	}
-	return sum; // Return the sum of non-negative values
+int sumNonNegative(vector<int>& arr, int size) {
+    int sum = 0; // Initialize a variable to store the sum
+    // Iterate through the elements of the array
+    for (int i = 0; i < size; i++) {
+        // Check if the current element is non-negative
+        if (arr[i] >= 0) {
+            // If the element is non-negative, add it to the sum
+            sum += arr[i];
+        }
+    }
+    return sum; // Return the sum of non-negative values
 }
 
 int main()
 {
-	int size;
-//Asks the user to enter the size of the list
-	cout << "Enter the size of the List: ";
-	cin >> size;
+    int size;
+    // Asks the user to enter the size of the list
+    cout << "Enter the size of the List: ";
+    cin >> size;
 
-	vector <int> list(size); //Declares the size of the arry at a  specified size
+    vector<int> list(size); // Declares the size of the array at a specified size
 
-//Prompts the user to enter the elements of the array
-	cout << "Enter" << size << " elements:" << std::endl;
-	for (int i = 0; i < size; i++) {
-		cin >> list[i]; //Reads the elements from the user
-	}
+    // Prompts the user to enter the elements of the array
+    cout << "Enter " << size << " elements:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        cin >> list[i]; // Reads the elements from the user
+    }
 
-//This calls the sumNonNegative function to calculate the sum of non-negative values
-	int result = sunNonNegative(list, size);
-	cout << "Sum of non-negative-values: " << result << std::endl;
+    // This calls the sumNonNegative function to calculate the sum of non-negative values
+    int result = sumNonNegative(list, size);
+    cout << "Sum of non-negative values: " << result << std::endl;
 
-
-	return 0;
+    return 0;
 }
-
